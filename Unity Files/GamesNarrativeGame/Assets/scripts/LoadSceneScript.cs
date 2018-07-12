2abcb4d76b3fe48779ac0c97b198d0efe85d4f53
Rowe026 <rowe026@gmail.com>
2018-07-12 16:13:11 +0100

    UIBasic

---
+++ Unity Files/GamesNarrativeGame/Assets/scripts/LoadSceneScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneScript : MonoBehaviour 
+{
+
+	// Loads scene1 (change name as apropreate).
+	public void MainScene () 
+	{
+		SceneManager.LoadScene ("demo 2");
+	}
+
+	// Loads scene2.
+	public void MenuScene () 
+	{
+		SceneManager.LoadScene ("MenuScene");
+	}
+		
+	// Loads scene2.
+	public void OptionsScene () 
+	{
+		SceneManager.LoadScene ("OptionScene");
+	}
+
+	// Quits the game.
+	public void Quit ()
+	{
+		Application.Quit ();
+	}
+}
