43de7f11fe6812411edc6032f23af9d6f1aa9ca3
Rowe026 <rowe026@gmail.com>
2018-07-12 14:59:00 +0100

    UIHacky

---
+++ Unity Files/GamesNarrativeGame/Assets/Scripts/LoadSceneScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneScript : MonoBehaviour 
+{
+
+	// Loads main game scene.
+	public void LoadMainScene () 
+	{
+		SceneManager.LoadScene ("demo 2");
+	}
+
+
+	// Loads main menu scene.
+	public void LoadMenuScene () 
+	{
+		SceneManager.LoadScene ("MenuScene");
+	}
+
+
+	// Loads options scene.
+	public void LoadOptionsScene () 
+	{
+		SceneManager.LoadScene ("OptionsScene");
+	}
+
+
+	// Quits the game.
+	public void Quit ()
+	{
+		Application.Quit ();
+	}
+}
---
+++ Unity Files/GamesNarrativeGame/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine (FadeScene ());
+	}
+
+
+	// Waits specified amount of seconds then loads main menu scene.
+	IEnumerator FadeScene()
+	{
+		yield return new WaitForSeconds (10);
+		SceneManager.LoadScene ("MenuScene");
+	}
+}
